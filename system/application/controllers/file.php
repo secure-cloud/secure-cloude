b7e0c2ecf38beb4f6173b2a77f945338d6ac961d
vishin-pavel <pavel@vishin.ru>
2012-03-05 12:24:23 +0400

    Для дебага

--- system/application/controllers/file.php
+++ system/application/controllers/file.php
@@ -21,7 +21,7 @@ class FileController extends \Abstracts\Controller{
 												$this->post->timestamp,
 											  2);
 			if($isSave !== true)
-				throw new Exception('error while saving file');
+				throw new Exception('error while saving file:'.$isSave->getMessage());
 			$this->view->json()->render('', array('status'=>'ok'));
 		} catch (Exception $e) {
 			$this->view->json()->render('', array('error'=>$e->getMessage()));
